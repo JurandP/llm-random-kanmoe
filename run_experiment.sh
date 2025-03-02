9c504658c6582f80a6ab552ee878e2b3a6517906
Mikołaj Dziok <m1kush@disroot.org>
2025-03-02 15:22:42 +0100

    add configs for additive/multiplicative/multiadd

--- run_experiment.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 -m lizrd.grid --config_path=configs/better_differential_transformer_none3.yaml --git_branch=diff_gda_adapters_grid_2025-02-22_19-46-35 --skip_copy_code --custom_backends_module=research.attention_moe.backends
